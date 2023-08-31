6cc97c99955d45c93249a1f2f6bdebb216fdfba6
Shadman352126 <shadman352126@gmail.com>
2023-08-31 12:26:06 +0600

    search option completed

--- js/phone.js
+++ js/phone.js
@@ -1,6 +1,6 @@
-const loadData = async () => {
+const loadData = async (searchText) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
@@ -9,11 +9,23 @@ const loadData = async () => {
 const displayPhone = (phones) => {
   // step-1:call the parent
   const phoneCardList = document.getElementById("phone-container");
+  // clear search result if you search again
+  phoneCardList.textContent = "";
+  // display show all phones
+  const showAllContainer = document.getElementById("show-all-container");
+  if (phones.length > 12) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
+  }
+  // display only 1st nth search
+  phones = phones.slice(0, 12);
+
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     // step-2:create the div
     const phoneCard = document.createElement("div");
-    phoneCard.classList = `card bg-base-100 shadow-xl`;
+    phoneCard.classList = `card bg-gray-100 m-6 shadow-xl`;
     // step-3:set inner html
     phoneCard.innerHTML = `
     <figure class="px-10 pt-10">
@@ -30,6 +42,25 @@ const displayPhone = (phones) => {
     // step-4:append child
     phoneCardList.appendChild(phoneCard);
   });
+  // hide loading
+  loaderSpinner(false);
 };
 
+// handle search button
+const handleSearch = () => {
+  loaderSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  // console.log(searchText);
+  loadData(searchText);
+  searchField.value = "";
+};
+const loaderSpinner = (loading) => {
+  const loadingSpinner = document.getElementById("spinner");
+  if (loading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
 loadData();
